357bde4dbf9c0d90b04a759127f14a9075353568
Arctur <mihailo.dilparic@gmail.com>
2022-03-18 15:54:56 +0100

    Added H1 tags and tailwind typography to editor

--- components/MDPreview.tsx
+++ components/MDPreview.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
-import { createEditor } from 'slate'
+import React, { useCallback, useState } from 'react'
+import { createEditor, Editor, Transforms } from 'slate'
 import { Editable, Slate, withReact } from 'slate-react'
 import { BaseEditor, Descendant } from 'slate'
 import { ReactEditor } from 'slate-react'
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomElement = {
+  type: 'paragraph' | 'header-one'
+  children: CustomText[]
+}
 type CustomText = { text: string }
 declare module 'slate' {
   interface CustomTypes {
@@ -18,6 +21,7 @@ type Props = {
 
 export default function MDPreview({ text }: Props) {
   const [editor] = useState(() => withReact(createEditor() as any))
+
   // Add the initial value when setting up our state.
   const [value, setValue] = useState<Descendant[]>([
     {
@@ -25,11 +29,39 @@ export default function MDPreview({ text }: Props) {
       children: [{ text: 'A line of text in a paragraph.' }],
     },
   ])
+  const ParagraphEl = (props: { attributes: any; children: any }) => {
+    return <p {...props.attributes}>{props.children}</p>
+  }
+  const HeaderOneEl = (props: { attributes: any; children: any }) => {
+    return <h1 {...props.attributes}>{props.children}</h1>
+  }
+
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case 'header-one':
+        return <HeaderOneEl {...props} />
+      default:
+        return <ParagraphEl {...props} />
+    }
+  }, [])
 
   return (
-    <div className="w-full">
+    <div className="prose m-0" style={{ maxWidth: '100vw' }}>
       <Slate editor={editor} value={value} onChange={setValue}>
-        <Editable />
+        <Editable
+          renderElement={renderElement}
+          onKeyDown={(event) => {
+            if (event.key === '1' && event.ctrlKey) {
+              console.log(event.key)
+              event.preventDefault()
+              Transforms.setNodes(
+                editor,
+                { type: 'header-one' },
+                { match: (n) => Editor.isBlock(editor, n) }
+              )
+            }
+          }}
+        />
       </Slate>
     </div>
   )
