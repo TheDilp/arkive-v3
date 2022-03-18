ec4ffca2b1aef59306e76ac351f03c4bcb7bf6a5
Arctur <mihailo.dilparic@gmail.com>
2022-03-18 13:23:14 +0100

    Initial Slate js setup completed

--- components/MDPreview.tsx
+++ components/MDPreview.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { createEditor } from 'slate'
+import { Editable, Slate, withReact } from 'slate-react'
+import { BaseEditor, Descendant } from 'slate'
+import { ReactEditor } from 'slate-react'
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
 type Props = {
   text: string
 }
 
 export default function MDPreview({ text }: Props) {
-  return <div>{text}</div>
+  const [editor] = useState(() => withReact(createEditor() as any))
+  // Add the initial value when setting up our state.
+  const [value, setValue] = useState<Descendant[]>([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+
+  return (
+    <div className="w-full">
+      <Slate editor={editor} value={value} onChange={setValue}>
+        <Editable />
+      </Slate>
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,14 @@ import MDPreview from '../components/MDPreview'
 const Home: NextPage = () => {
   const [text, setText] = useState<string>('# This is a [[test]]')
   return (
-    <div className="flex min-h-screen items-center justify-center py-2">
+    <div className="flex min-h-screen w-screen items-start">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="max-w-8xl flex w-full items-center justify-center text-center">
-        <div className="w-1/2 bg-green-400">
-          <textarea value={text} onChange={(e) => setText(e.target.value)} />
-        </div>
-        <div className="prose w-1/2 bg-blue-400">
+      <main className="w-full text-center">
+        <div className="w-full bg-blue-400">
           <MDPreview text={text} />
         </div>
       </main>
