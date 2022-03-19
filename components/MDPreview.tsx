4cfe7c3eb2d783f19930f5325925bd65fa855a30
Arctur <mihailo.dilparic@gmail.com>
2022-03-19 09:34:00 +0100

    Added header and select-all shortcuts to editor

--- components/MDPreview.tsx
+++ components/MDPreview.tsx
@@ -4,7 +4,7 @@ import { Editable, Slate, withReact } from 'slate-react'
 import { BaseEditor, Descendant } from 'slate'
 import { ReactEditor } from 'slate-react'
 type CustomElement = {
-  type: 'paragraph' | 'header-one'
+  type: string
   children: CustomText[]
 }
 type CustomText = { text: string }
@@ -35,15 +35,48 @@ export default function MDPreview({ text }: Props) {
   const HeaderOneEl = (props: { attributes: any; children: any }) => {
     return <h1 {...props.attributes}>{props.children}</h1>
   }
+  const HeaderTwoEl = (props: { attributes: any; children: any }) => {
+    return <h2 {...props.attributes}>{props.children}</h2>
+  }
+  const HeaderThreeEl = (props: { attributes: any; children: any }) => {
+    return <h3 {...props.attributes}>{props.children}</h3>
+  }
+  const HeaderFourEl = (props: { attributes: any; children: any }) => {
+    return <h4 {...props.attributes}>{props.children}</h4>
+  }
+  const HeaderFiveEl = (props: { attributes: any; children: any }) => {
+    return <h5 {...props.attributes}>{props.children}</h5>
+  }
+  const HeaderSixEl = (props: { attributes: any; children: any }) => {
+    return <h6 {...props.attributes}>{props.children}</h6>
+  }
 
   const renderElement = useCallback((props) => {
     switch (props.element.type) {
       case 'header-one':
         return <HeaderOneEl {...props} />
+      case 'header-two':
+        return <HeaderTwoEl {...props} />
+      case 'header-three':
+        return <HeaderThreeEl {...props} />
+      case 'header-four':
+        return <HeaderFourEl {...props} />
+      case 'header-five':
+        return <HeaderFiveEl {...props} />
+      case 'header-six':
+        return <HeaderSixEl {...props} />
       default:
         return <ParagraphEl {...props} />
     }
   }, [])
+
+  function transformHeader(type: string) {
+    Transforms.setNodes(
+      editor,
+      { type },
+      { match: (n) => Editor.isBlock(editor, n) }
+    )
+  }
 
   return (
     <div className="prose m-0" style={{ maxWidth: '100vw' }}>
@@ -51,14 +84,30 @@ export default function MDPreview({ text }: Props) {
         <Editable
           renderElement={renderElement}
           onKeyDown={(event) => {
-            if (event.key === '1' && event.ctrlKey) {
-              console.log(event.key)
+            if (event.ctrlKey) {
+              // Select All
+              if (event.key === 'a') {
+                Transforms.select(editor, {
+                  anchor: Editor.start(editor, []),
+                  focus: Editor.end(editor, []),
+                })
+                return
+              }
               event.preventDefault()
-              Transforms.setNodes(
-                editor,
-                { type: 'header-one' },
-                { match: (n) => Editor.isBlock(editor, n) }
-              )
+              //   Headers
+              if (event.key === '1') {
+                transformHeader('header-one')
+              } else if (event.key === '2') {
+                transformHeader('header-two')
+              } else if (event.key === '3') {
+                transformHeader('header-three')
+              } else if (event.key === '4') {
+                transformHeader('header-four')
+              } else if (event.key === '5') {
+                transformHeader('header-five')
+              } else if (event.key === '6') {
+                transformHeader('header-six')
+              }
             }
           }}
         />
