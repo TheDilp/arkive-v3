05140e59f959293346b1362ccf9d5d9d0d04bd91
Arctur <mihailo.dilparic@gmail.com>
2022-03-19 13:01:39 +0100

    Updated mention tag styling

--- components/Mention.tsx
+++ components/Mention.tsx
@@ -12,16 +12,11 @@ export default function Mention({ attributes, children, element }: Props) {
   const focused = useFocused()
   return (
     <span
+      className=" cursor-pointer rounded bg-blue-400 p-0.5 text-base text-white transition-colors hover:bg-blue-300 hover:text-gray-800"
       {...attributes}
       contentEditable={false}
       data-cy={`mention-${element.character.replace(' ', '-')}`}
       style={{
-        padding: '3px 3px 2px',
-        margin: '0 1px',
-        display: 'inline-block',
-        borderRadius: '4px',
-        backgroundColor: '#eee',
-        fontSize: '0.9em',
         boxShadow: selected && focused ? '0 0 0 2px #B4D5FF' : 'none',
       }}
     >
