ea5e0a4576ce58c304634b35312eb16449a84d69
Arctur <mihailo.dilparic@gmail.com>
2022-03-21 09:47:14 +0100

    Added basic styling to login page / fixed .env extension

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -8,7 +8,7 @@ export default function login({}: Props) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleLogin = async (email: string) => {
+  const handleLogin = async (email: string, password: string) => {
     try {
       setLoading(true)
       const { error } = await supabase.auth.signIn({ email, password })
@@ -22,19 +22,39 @@ export default function login({}: Props) {
   }
 
   return (
-    <div>
-      <input
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex max-w-6xl flex-col space-y-4 rounded p-20 shadow">
+        <div className="w-full text-center">
+          <h1 className="text-2xl">Welcome back</h1>
+          <h2 className="text-lg">Login</h2>
+        </div>
+        <div className="flex w-full justify-center">
+          <input
+            className="w-64 focus:outline-blue-500"
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="flex w-full justify-center">
+          <input
+            type="password"
+            className="w-64 focus:outline-blue-500"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex w-full justify-center">
+          <button
+            onClick={() => handleLogin(email, password)}
+            className="w-20 rounded bg-blue-400 p-2 text-white shadow-sm"
+          >
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
