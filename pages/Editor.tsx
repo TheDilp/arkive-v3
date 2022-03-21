7f532698b06fd53c2b4b3d0820d04a0d046a3b2a
Arctur <mihailo.dilparic@gmail.com>
2022-03-21 10:17:22 +0100

    Added basic navbar and styling

--- pages/Editor.tsx
+++ pages/Editor.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import EditorComponent from '../components/EditorComponent'
+import Navbar from '../components/Navbar'
 
 type Props = {}
 
 export default function Editor({}: Props) {
   return (
-    <div className="mt-2 flex justify-center ">
-      <div className="w-2/3  rounded p-4 shadow-lg">
-        <EditorComponent />
+    <div>
+      <Navbar />
+
+      <div className="mt-2 flex justify-center ">
+        <div className="w-2/3  rounded p-4 shadow-lg">
+          <EditorComponent content={null} />
+        </div>
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen w-screen items-start">
@@ -7,7 +8,7 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar />
       <main className="w-full text-center">MAIN PAGE</main>
     </div>
   )
