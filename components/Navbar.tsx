8d75f199c97636dee2ee5e5b6ba72b0fdead5e99
Arctur <mihailo.dilparic@gmail.com>
2022-03-23 13:27:28 +0100

    Added basic home page
    This page will contain projects for the current user that they can select

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
@@ -5,9 +6,10 @@ type Props = {}
 export default function Navbar({}: Props) {
   return (
     <nav className="flex items-center justify-between space-x-4 bg-neutral-700 py-2 text-white shadow">
-      <div className="cursor-pointer pl-2">Logo</div>
       <div className="mx-auto flex">
-        <span className="mx-auto cursor-pointer text-4xl">ArKive</span>
+        <span className="mx-auto cursor-pointer text-4xl">
+          <Link href="/">ArKive</Link>
+        </span>
       </div>
       <div className="flex justify-between space-x-2 pr-2">
         <span className="cursor-pointer">Profile</span>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import Navbar from '../components/Navbar'
 const Home: NextPage = () => {
   return (
@@ -10,6 +12,42 @@ const Home: NextPage = () => {
       </Head>
       <main className="w-full text-center">
         <Navbar />
+        <div className="mt-2 flex h-screen justify-center space-x-1">
+          <Link href="/Editor">
+            <div className="group h-96 w-1/4 cursor-pointer">
+              <div className="h-full  w-5/6 rounded bg-white shadow">
+                <div className="relative  h-3/4 w-full">
+                  <Image
+                    src={'/projects.jpg'}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-t transition-transform group-hover:scale-110"
+                  />
+                </div>
+                <div className="flex h-1/4 w-full items-center justify-center">
+                  <h2 className="truncate text-2xl">Projects</h2>
+                </div>
+              </div>
+            </div>
+          </Link>
+          <Link href="/Editor">
+            <div className="group h-96 w-1/4 cursor-pointer">
+              <div className="h-full  w-5/6 rounded bg-white shadow">
+                <div className="relative  h-3/4 w-full">
+                  <Image
+                    src={'/randompage.jpg'}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-t transition-transform group-hover:scale-110"
+                  />
+                </div>
+                <div className="flex h-1/4 w-full items-center justify-center">
+                  <h2 className="truncate text-2xl">Random Page</h2>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
       </main>
     </div>
   )
