d95bd0c3dfbf1ec864241be81003b1b0974781bc
Arctur <mihailo.dilparic@gmail.com>
2022-04-02 17:36:42 +0200

    Removed NextJS entirely

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['picsum.photos', 'upload.wikimedia.org', 'i.pinimg.com'],
-  },
-}
--- pages/article/[slug].tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {
-  text: string
-}
-
-export default function Article({ text }: Props) {
-  return <div>{text}</div>
-}
