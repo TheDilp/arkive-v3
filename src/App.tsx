d27b5b9a64d92d93e61174d6d7e66cf6fb513d19
Arctur <mihailo.dilparic@gmail.com>
2022-04-04 09:21:29 +0200

    Got cursor position and menu to be at its location

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 function App() {
-  const [state, setState] = useState("");
+  const [state, setState] = useState({ text: "", lastWord: "" });
   const [menuState, setMenuState] = useState({ show: false, x: 0, y: 0 });
   const menu = ["ITEM1", "ITEM2", "ITEM3"];
+  const inputRef = useRef() as React.MutableRefObject<HTMLDivElement>;
   return (
     <main className="App">
       <div id="editor">
-        <textarea
-          value={state}
-          onChange={(e) => setState(e.target.value)}
+        <div
+          contentEditable={true}
+          ref={inputRef}
+          onChange={(e) =>
+            setState({
+              text: e.currentTarget.innerText,
+              lastWord: e.currentTarget.innerText,
+            })
+          }
           onKeyDown={(e) => {
+            //  Create dropdown menu on slash command in textarea
             if (e.key === "/") {
-              console.log(e.currentTarget.selectionStart);
-              // setMenuState({show: true, })
+              const range = window.getSelection()?.getRangeAt(0).cloneRange();
+              range?.collapse(true);
+              const rect = range?.getClientRects()[0];
+              console.log(rect);
+              if (rect) setMenuState({ show: true, x: rect?.x, y: rect?.y });
             }
+
+            // if (state.text.)
+            // if (e.key === "/") {
+            //   setMenuState({ ...menuState, show: true });
+            // } else if (e.key === "Space" || e.key === "Enter") {
+            //   setState({ ...state, lastWord: "" });
+            // }
           }}
-        ></textarea>
+        ></div>
         {menuState.show && (
-          <ul>
+          <ul
+            style={{
+              position: "absolute",
+              top: menuState.y,
+              left: menuState.x,
+            }}
+          >
             {menu.map((item) => (
               <li>{item}</li>
             ))}
