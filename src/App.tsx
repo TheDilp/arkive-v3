0b7c509cab23ac6709485a06e6ffd5f1cab9660d
Arctur <mihailo.dilparic@gmail.com>
2022-04-04 08:58:19 +0200

    Started work on custom textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,30 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import EditorJS from "@editorjs/editorjs";
-import Header from "@editorjs/header";
-import LinkAutoComplete from "@editorjs/link-autocomplete";
-import ContentAlignmentTool from "editorjs-text-alignment-blocktune";
 function App() {
-  const editorRef = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const [editor, setEditor] = useState<EditorJS | null>(null);
-
-  useEffect(() => {
-    if (editorRef && editorRef.current) {
-      if (!editor)
-        setEditor(
-          new EditorJS({
-            holder: editorRef.current,
-            tools: {
-              header: { class: Header, tunes: ["anyTuneName"] },
-              linkTool: {
-                class: LinkAutoComplete,
-                tunes: ["anyTuneName"],
-                config: {
-                  // endpoint: "youtube.com", // Your backend endpoint for url data fetching,
-                },
-              },
-              anyTuneName: {
-                class: ContentAlignmentTool,
-                config: {
-                  default: "left",
-                  blocks: {
-                    header: "left",
-                    list: "left",
-                  },
-                },
-              },
-            },
-          })
-        );
-    }
-    return () => {
-      if (editor) editor.destroy();
-    };
-  }, [editorRef.current]);
-
+  const [state, setState] = useState("");
+  const [menuState, setMenuState] = useState({ show: false, x: 0, y: 0 });
+  const menu = ["ITEM1", "ITEM2", "ITEM3"];
   return (
     <main className="App">
-      <div id="editor" ref={editorRef}></div>
+      <div id="editor">
+        <textarea
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "/") {
+              console.log(e.currentTarget.selectionStart);
+              // setMenuState({show: true, })
+            }
+          }}
+        ></textarea>
+        {menuState.show && (
+          <ul>
+            {menu.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </main>
   );
 }
--- src/custom-modules.d.ts
+++ src/custom-modules.d.ts
@@ -1,3 +1,7 @@
 declare module "@editorjs/header";
 declare module "@editorjs/link-autocomplete";
 declare module "editorjs-text-alignment-blocktune";
+declare module "linkify-it";
+declare module "markdown-it";
+declare module "react-linkify";
+declare module "@webscopeio/react-textarea-autocomplete";
