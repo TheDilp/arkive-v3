ccf5169c7e10fce56221240e1abdceb5bd126a2a
Arctur <mihailo.dilparic@gmail.com>
2022-04-05 15:39:39 +0200

    Reset for Remirror attempt

--- src/custom-modules.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@editorjs/header";
-declare module "@editorjs/link-autocomplete";
-declare module "editorjs-text-alignment-blocktune";
-declare module "linkify-it";
-declare module "markdown-it";
-declare module "react-linkify";
-declare module "@webscopeio/react-textarea-autocomplete";
-declare module "showdown";
