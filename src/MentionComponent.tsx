361e191987e9479fc13c9c10885f0315dee3fb23
Arctur <mihailo.dilparic@gmail.com>
2022-04-05 17:05:40 +0200

    Init commit WITH MENTION EXTENSION WORKING!!!!

---
+++ src/MentionComponent.tsx
@@ -0,0 +1,38 @@
+import { MentionAtomPopupComponent, MentionAtomState } from "@remirror/react";
+import React, { useMemo, useState } from "react";
+import { MentionAtomNodeAttributes } from "remirror/extensions";
+
+type Props = {};
+interface MentionComponentProps<
+  UserData extends MentionAtomNodeAttributes = MentionAtomNodeAttributes
+> {
+  documents?: {
+    id: string;
+    label: string;
+  }[];
+}
+export default function MentionComponent({ documents }: MentionComponentProps) {
+  const [mentionState, setMentionState] = useState<MentionAtomState | null>();
+  const documentItems = useMemo(
+    () =>
+      (documents ?? []).map((doc) => ({
+        id: doc.id,
+        label: `#${doc.label}`,
+        href: doc.id,
+      })),
+    [documents]
+  );
+  const items = useMemo(() => {
+    if (!mentionState) {
+      return [];
+    }
+
+    const query = mentionState.query.full.toLowerCase() ?? "";
+    return documentItems
+      .filter((item) => item.label.toLowerCase().includes(query))
+      .sort();
+  }, [mentionState, documentItems]);
+
+  //   @ts-ignore
+  return <MentionAtomPopupComponent onChange={setMentionState} items={items} />;
+}
