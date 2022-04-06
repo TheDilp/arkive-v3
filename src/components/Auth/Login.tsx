e77525488e35102f2fa388c64e5149f88bdc453d
Arctur <mihailo.dilparic@gmail.com>
2022-04-06 13:20:12 +0200

    Finished basic login page styling

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import images from "./authImages";
 import { user, login } from "../../utils/supabaseUtils";
 import "../../styles/Login.css";
+import { Navigate, useNavigate } from "react-router-dom";
 type Props = {};
 
 export default function Login({}: Props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
   const [index, set] = useState(0);
   useEffect(() => {
     let timeout = setTimeout(() => {
@@ -19,7 +20,9 @@ export default function Login({}: Props) {
     }, 5000);
     return () => clearTimeout(timeout);
   }, [index]);
-  return (
+  return user && user.id ? (
+    <Navigate to="/" />
+  ) : (
     <div className="loginPageContainer">
       <div className="loginCard">
         <div className="loginCardImage">
@@ -34,7 +37,43 @@ export default function Login({}: Props) {
           ))}
         </div>
         <div className="loginCardForm">
-            
+          <div>
+            <h1 className="loginCardFormTitle">Welcome Back</h1>
+            <h3 className="loginCardFormSubtitle">
+              Don't have an account? <a href="/register">Register Today!</a>
+            </h3>
+          </div>
+          <div className="loginCardFormInputContainer">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="loginCardFormInputContainer">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="loginCardFormButtonContainer">
+            <button
+              className="loginCardFormButton"
+              onClick={async () => {
+                await login(email, password);
+                navigate("/");
+              }}
+            >
+              Sign In
+            </button>
+          </div>
         </div>
       </div>
     </div>
