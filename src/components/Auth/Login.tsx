b5f0c9db6ff04cdaeaedf9ff508a0e126a2730f9
Arctur <mihailo.dilparic@gmail.com>
2022-04-06 13:00:41 +0200

    Basic login card with changing images

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import images from "./authImages";
 import { user, login } from "../../utils/supabaseUtils";
+import "../../styles/Login.css";
 type Props = {};
 
 export default function Login({}: Props) {
@@ -19,86 +20,21 @@ export default function Login({}: Props) {
     return () => clearTimeout(timeout);
   }, [index]);
   return (
-    <div className="w-full h-full flex align-items-center justify-content-center">
-      <div className="surface-card shadow-4 w-full border-round w-6 flex">
-        <div className="w-7 relative">
-          <h1
-            className="text-center text-6xl text-white absolute w-full z-5"
-            style={{
-              textShadow: "0 0 20px black",
-            }}
-          >
-            Discover your world
-          </h1>
+    <div className="loginPageContainer">
+      <div className="loginCard">
+        <div className="loginCardImage">
           {images.map((image, imgIDX) => (
             <img
               key={imgIDX}
               src={image.url}
-              className="w-full h-full absolute transition-all transition-duration-200"
+              className=""
               alt="planet placeholder"
-              style={{ objectFit: "cover", opacity: index === imgIDX ? 1 : 0 }}
+              style={{ opacity: index === imgIDX ? 1 : 0 }}
             />
           ))}
         </div>
-        <div className="w-5 p-4">
-          <div className="text-center mb-5">
-            <div className="text-900 text-3xl font-medium mb-3 ">
-              Welcome Back
-            </div>
-            <span className="text-600 font-medium line-height-3">
-              Don't have an account?
-            </span>
-            <a
-              href="/register"
-              className="font-medium no-underline ml-2 text-blue-500 cursor-pointer robotoFont"
-            >
-              Register today!
-            </a>
-          </div>
-
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-900 font-medium mb-2 robotoFont"
-            >
-              Email
-            </label>
-            <input
-              id="email"
-              type="text"
-              className="w-full mb-3"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-
-            <label
-              htmlFor="password"
-              className="block text-900 font-medium mb-2 robotoFont"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="w-full mb-3"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  login(email, password);
-                }
-              }}
-            />
-
-            <button
-              className="w-full text-white robotoFont"
-              onClick={() => login(email, password)}
-            >
-              Sign In
-            </button>
-          </div>
+        <div className="loginCardForm">
+            
         </div>
       </div>
     </div>
