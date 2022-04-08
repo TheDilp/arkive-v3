c8c31749b2b21374907c4127e9a36c7ee229b43d
Arctur <mihailo.dilparic@gmail.com>
2022-04-08 10:41:03 +0200

    Replaced webpack with vite

--- src/utils/supabaseUtils.ts
+++ src/utils/supabaseUtils.ts
@@ -3,8 +3,9 @@ import { RemirrorJSON } from "remirror";
 import { Document, Project } from "../custom-types";
 import { toastError } from "./utils";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log(import.meta.env);
 export const supabase = createClient(
   supabaseUrl as string,
   supabaseKey as string
