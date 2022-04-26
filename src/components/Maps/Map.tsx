0361868f5d6f0324e74f005d75dbf51c8fc8c4ef
Arctur <mihailo.dilparic@gmail.com>
2022-04-26 13:26:44 +0200

    Added transition animation when changing maps

--- src/components/Maps/Map.tsx
+++ src/components/Maps/Map.tsx
@@ -3,35 +3,45 @@ import Map from "ol/Map";
 import { Projection } from "ol/proj";
 import Static from "ol/source/ImageStatic";
 import View from "ol/View";
-import { useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
 import { Map as MapType } from "../../custom-types";
 import LoadingScreen from "../Util/LoadingScreen";
-
+import { motion, AnimatePresence } from "framer-motion";
 export default function MapView() {
   const { project_id, map_id } = useParams();
   const queryClient = useQueryClient();
   const mapRef = useRef() as React.MutableRefObject<HTMLDivElement>;
   const [imageData, setImageData] = useState({ width: 0, height: 0, src: "" });
-
-  useLayoutEffect(() => {
-    const maps: MapType[] | undefined = queryClient.getQueryData(
-      `${project_id}-maps`
-    );
+  const [test, setTest] = useState(true);
+  const maps: MapType[] | undefined = queryClient.getQueryData(
+    `${project_id}-maps`
+  );
+  useEffect(() => {
+    setTest(false);
     if (maps) {
       const map = maps.find((m) => m.id === map_id);
       if (map) {
-        let img = new Image();
-        img.src = map.map_image;
-        img.onload = () =>
+        setTimeout(() => {
+          let img = new Image();
+          img.src = map.map_image;
+          img.onload = () =>
+            setImageData({
+              width: img.width,
+              height: img.height,
+              src: img.src,
+            });
           setImageData({ width: img.width, height: img.height, src: img.src });
-        setImageData({ width: img.width, height: img.height, src: img.src });
+          setTest(true);
+        }, 750);
       }
     }
+
+    // return () => clearTimeout(timeout);
   }, [map_id]);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (imageData.width && imageData.height) {
       const extent = [0, 0, imageData.width, imageData.height];
       const projection = new Projection({
@@ -64,10 +74,22 @@ export default function MapView() {
       return () => mapp.dispose();
     }
   }, [imageData]);
-
   return (
-    <div id="map" ref={mapRef} className="w-10 h-screen">
-      <LoadingScreen />
+    <div className="w-10 h-screen">
+      <AnimatePresence exitBeforeEnter>
+        {test && imageData.src && (
+          <motion.div
+            id="map"
+            key={map_id}
+            ref={mapRef}
+            className="w-full h-full"
+            transition={{ ease: "easeInOut", duration: 0.5 }}
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
