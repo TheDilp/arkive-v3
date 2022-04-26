8a0e60a4f4f52e13e28a3f4d0e6d5444d0699226
Arctur <mihailo.dilparic@gmail.com>
2022-04-26 18:13:32 +0200

    Switched back to leaflet

--- src/components/Maps/Map.tsx
+++ src/components/Maps/Map.tsx
@@ -1,130 +1,77 @@
-import ImageLayer from "ol/layer/Image";
-import Map from "ol/Map";
-import Feature from "ol/Feature";
-import Point from "ol/geom/Point";
-import { Projection } from "ol/proj";
-import Static from "ol/source/ImageStatic";
-import View from "ol/View";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
-import { Map as MapType } from "../../custom-types";
-import { Icon, Style } from "ol/style";
-import VectorSource from "ol/source/Vector";
-import { Vector as VectorLayer } from "ol/layer";
-import { motion, AnimatePresence } from "framer-motion";
-import MapContextMenu from "./MapContextMenu";
+import { Map } from "../../custom-types";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  ImageOverlay,
+} from "react-leaflet";
+import L, { LatLngBoundsExpression } from "leaflet";
 export default function MapView() {
   const { project_id, map_id } = useParams();
   const queryClient = useQueryClient();
   const cm = useRef(null);
-  const mapRef = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const [imageData, setImageData] = useState({ width: 0, height: 0, src: "" });
-  const [test, setTest] = useState(true);
-  const maps: MapType[] | undefined = queryClient.getQueryData(
+  const imgRef = useRef() as any;
+  const [mapData, setMapData] = useState({ width: 0, height: 0, src: "" });
+  const [bounds, setBounds] = useState<LatLngBoundsExpression>([
+    [0, 0],
+    [0, 0],
+  ]);
+  const maps: Map[] | undefined = queryClient.getQueryData(
     `${project_id}-maps`
   );
   useEffect(() => {
-    setTest(false);
-    if (maps) {
-      const map = maps.find((m) => m.id === map_id);
+    if (map_id) {
+      const map = maps?.find((m) => m.id === map_id);
       if (map) {
-        setTimeout(() => {
-          setTest(true);
-          let img = new Image();
-          img.src = map.map_image;
-          img.onload = () =>
-            setImageData({
-              width: img.width,
-              height: img.height,
-              src: img.src,
-            });
-          setImageData({ width: img.width, height: img.height, src: img.src });
-        }, 750);
+        let img = new Image();
+        img.src = map.map_image;
+        img.onload = () => {
+          setBounds([
+            [0 - img.height, 0 - img.width],
+            [img.height, img.width],
+          ]);
+          setMapData({
+            width: img.width,
+            height: img.height,
+            src: map.map_image,
+          });
+        };
       }
     }
   }, [map_id]);
 
   useEffect(() => {
-    if (imageData.width && imageData.height) {
-      const extent = [0, 0, imageData.width, imageData.height];
-      const projection = new Projection({
-        code: "whatevz",
-        units: "m",
-        extent: extent,
-        worldExtent: extent,
-      });
-      const iconFeature = new Feature({
-        geometry: new Point([0, 0]),
-        name: "Null Island",
-        population: 4000,
-        rainfall: 500,
-      });
-
-      const iconStyle = new Style({
-        image: new Icon({
-          src: "https://api.iconify.design/mdi/wizard-hat.svg?color=white",
-          anchor: [0, 0],
-          anchorXUnits: "fraction",
-          anchorYUnits: "pixels",
-        }),
-      });
-
-      iconFeature.setStyle(iconStyle);
-
-      const vectorSource = new VectorSource({
-        features: [iconFeature],
-      });
-
-      const vectorLayer = new VectorLayer({
-        source: vectorSource,
-      });
-      let mapp = new Map({
-        target: mapRef.current || "map",
-        layers: [
-          new ImageLayer({
-            source: new Static({
-              url: imageData.src,
-              imageExtent: extent,
-            }),
-          }),
-          vectorLayer,
-        ],
-
-        view: new View({
-          resolution: 1,
-          center: [imageData.width / 2, imageData.height / 2],
-          projection,
-          maxZoom: 5,
-          multiWorld: false,
-        }),
-      });
-
-      return () => mapp.dispose();
+    if (imgRef.current) {
+      imgRef.current.setBounds([
+        [0 - mapData.height, 0 - mapData.width],
+        [mapData.height, mapData.width],
+      ]);
     }
-  }, [imageData]);
+  }, [bounds]);
   return (
     <div className="w-10 h-screen">
-      <MapContextMenu cm={cm} />
-      <AnimatePresence exitBeforeEnter>
-        {test && imageData.src && (
-          <motion.div
-            onContextMenu={(e) => {
-              e.preventDefault();
-              //@ts-ignore
-              cm.current.show(e);
-            }}
-            id="map"
-            key={map_id}
-            ref={mapRef}
-            className="w-full h-full"
-            transition={{ ease: "easeInOut", duration: 0.5 }}
-            initial={{ opacity: 1 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          ></motion.div>
-        )}
-      </AnimatePresence>
+      {mapData.width && mapData.height && (
+        <MapContainer
+          className="w-full h-full bg-gray-900"
+          center={[mapData.width / 2, mapData.height / 2]}
+          zoom={0}
+          minZoom={-3}
+          scrollWheelZoom={true}
+          crs={L.CRS.Simple}
+          bounds={bounds}
+        >
+          <ImageOverlay url={mapData.src} bounds={bounds} ref={imgRef} />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </div>
   );
 }
