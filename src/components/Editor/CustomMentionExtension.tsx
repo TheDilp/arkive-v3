84df48cb9d752b255b54b33be8bb0926b91f9991
Arctur <mihailo.dilparic@gmail.com>
2022-05-02 13:17:36 +0200

    Map markers have icon and link to map page

--- src/components/Editor/CustomMentionExtension.tsx
+++ src/components/Editor/CustomMentionExtension.tsx
@@ -1,8 +1,9 @@
+import { Icon } from "@iconify/react";
 import { useEffect } from "react";
 import { useQueryClient } from "react-query";
 import { Link, useParams } from "react-router-dom";
 import { MentionAtomExtension } from "remirror/extensions";
-import { Document } from "../../custom-types";
+import { Document, Map } from "../../custom-types";
 
 const CustomMentionExtension = new MentionAtomExtension({
   extraTags: ["link"],
@@ -27,15 +28,21 @@ CustomMentionExtension.ReactComponent = ({ node }) => {
   const queryClient = useQueryClient();
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { project_id } = useParams();
-  const docs: Document[] | undefined = queryClient.getQueryData(
-    `${project_id}-documents`
-  );
-  const doc = docs ? docs.find((doc) => doc.id === node.attrs.id) : null;
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {}, [docs]);
+
+  let item: Document | Map | undefined;
+  if (node.attrs.name === "at") {
+    const items = queryClient.getQueryData<Document[]>(
+      `${project_id}-documents`
+    );
+    item = items?.find((doc) => doc.id === node.attrs.id);
+  } else {
+    const items = queryClient.getQueryData<Map[]>(`${project_id}-maps`);
+    item = items?.find((map) => map.id === node.attrs.id);
+  }
+
   return (
     <Link
-      className="Lato text-white"
+      className="Lato text-white "
       style={{
         fontWeight: "700",
       }}
@@ -45,7 +52,12 @@ CustomMentionExtension.ReactComponent = ({ node }) => {
           : `../../maps/${node.attrs.id}`
       }
     >
-      {doc ? doc?.title : node.attrs.label}
+      {node.attrs.name === "hash" ? (
+        <Icon icon="mdi:map-marker" className="underline" />
+      ) : (
+        ""
+      )}
+      {item ? item?.title : node.attrs.label}
     </Link>
   );
 };
