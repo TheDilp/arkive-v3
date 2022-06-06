cc208fd5c8edb43c052a03c48247c74f80b92178
Arctur <mihailo.dilparic@gmail.com>
2022-06-06 18:59:54 +0200

    Added parent select for creating from template

--- src/components/Wiki/DocumentTree/TreeFilter/DocumentFromTempDialog.tsx
+++ src/components/Wiki/DocumentTree/TreeFilter/DocumentFromTempDialog.tsx
@@ -1,15 +1,17 @@
+import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
-import React, { useState } from "react";
+import { Dropdown } from "primereact/dropdown";
+import { InputText } from "primereact/inputtext";
+import { RadioButton } from "primereact/radiobutton";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+import { DocumentCreateProps, DocumentProps } from "../../../../custom-types";
 import {
   useCreateDocument,
+  useGetDocuments,
   useGetTemplates,
 } from "../../../../utils/customHooks";
-import { RadioButton } from "primereact/radiobutton";
-import { Button } from "primereact/button";
-import { v4 as uuid } from "uuid";
-import { InputText } from "primereact/inputtext";
-import { DocumentProps } from "../../../../custom-types";
 type Props = {
   visible: boolean;
   setVisible: (visible: boolean) => void;
@@ -17,22 +19,25 @@ type Props = {
 
 export default function DocumentFromTempDialog({ visible, setVisible }: Props) {
   const { project_id } = useParams();
-  const [value, setValue] = useState();
+  const [selectedTemplate, setSelectedTemplate] =
+    useState<DocumentProps | null>(null);
+  const [selectedParent, setSelectedParent] = useState<string | null>(null);
   const [customName, setCustomName] = useState<string | undefined>();
-  const templates = useGetTemplates(project_id as string);
+  const { data: documents } = useGetDocuments(project_id as string);
   const createDocumentMutation = useCreateDocument(project_id as string);
-
   function createDocumentFromTemplate(
     template: DocumentProps,
+    parent: string | null,
     customName: string | undefined
   ) {
     let id = uuid();
     createDocumentMutation.mutate({
       ...template,
       template: false,
+      folder: false,
       id,
-      parent: null,
       title: customName || template.title,
+      parent,
     });
   }
   return (
@@ -41,27 +46,29 @@ export default function DocumentFromTempDialog({ visible, setVisible }: Props) {
       header={"Create Document From Template"}
       visible={visible}
       onHide={() => {
-        setValue(undefined);
+        setSelectedTemplate(null);
         setVisible(false);
       }}
     >
       <h3 className="Lato">Select a template</h3>
       <div className="field-radiobutton flex flex-wrap align-items-start h-5rem overflow-y-auto">
-        {templates &&
-          templates.map((template) => (
-            <div
-              className="w-6 my-1 Lato flex align-items-center"
-              key={template.id}
-            >
-              <RadioButton
-                className="mr-2"
-                value={template.id}
-                onChange={(e) => setValue(e.value)}
-                checked={value === template.id}
-              />
-              {template.title}
-            </div>
-          ))}
+        {documents &&
+          documents
+            .filter((doc) => doc.template)
+            .map((template) => (
+              <div
+                className="w-6 my-1 Lato flex align-items-center"
+                key={template.id}
+              >
+                <RadioButton
+                  className="mr-2"
+                  value={template.id}
+                  onChange={(e) => setSelectedTemplate(template)}
+                  checked={selectedTemplate?.id === template.id}
+                />
+                {template.title}
+              </div>
+            ))}
       </div>
       <div className="w-full my-2">
         <InputText
@@ -71,12 +78,36 @@ export default function DocumentFromTempDialog({ visible, setVisible }: Props) {
           onChange={(e) => setCustomName(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
-              let template = templates.find((temp) => temp.id === value);
-              if (template) {
-                createDocumentFromTemplate(template, customName);
+              if (selectedTemplate) {
+                createDocumentFromTemplate(
+                  selectedTemplate,
+                  selectedParent,
+                  customName
+                );
               }
             }
           }}
+        />
+      </div>
+      <div className="w-full my-2">
+        <Dropdown
+          className="w-full"
+          placeholder="Document Folder"
+          optionLabel="title"
+          optionValue="id"
+          value={selectedParent}
+          filter
+          onChange={(e) => setSelectedParent(e.value)}
+          options={
+            documents
+              ? [
+                  { title: "Root", id: null },
+                  ...documents.filter((doc) => {
+                    return doc.folder && !doc.template;
+                  }),
+                ]
+              : []
+          }
         />
       </div>
       <div className="w-full flex justify-content-end">
@@ -86,9 +117,12 @@ export default function DocumentFromTempDialog({ visible, setVisible }: Props) {
           icon="pi pi-copy"
           iconPos="right"
           onClick={() => {
-            let template = templates.find((temp) => temp.id === value);
-            if (template) {
-              createDocumentFromTemplate(template, customName);
+            if (selectedTemplate) {
+              createDocumentFromTemplate(
+                selectedTemplate,
+                selectedParent,
+                customName
+              );
             }
           }}
         />
