5b97de0e122b27debbbfeca1f1db40c419992fae
Arctur <mihailo.dilparic@gmail.com>
2022-07-21 12:18:07 +0200

    Fixed slash menu blocking cursor

--- src/components/Wiki/Editor/CommandMenu.tsx
+++ src/components/Wiki/Editor/CommandMenu.tsx
@@ -1,52 +1,80 @@
 import {
   FloatingWrapper,
+  useChainedCommands,
   useCommands,
   useMenuNavigation,
+  useSelectedText,
   useSuggest,
 } from "@remirror/react";
 import { useCallback } from "react";
 
 export function CommandMenu() {
-  const commands = useCommands();
+  const chain = useChainedCommands();
   const { change } = useSuggest({
     char: "/",
     name: "command",
   });
-  const enabled = Boolean(change);
 
+  const enabled = Boolean(change?.match[1]);
+  console.log(enabled);
   const range = change?.range;
   const onSubmit = useCallback(
     (cmd) => {
-      console.log("Do something");
+      if (cmd.type === "heading") {
+        chain.toggleHeading({ level: cmd.level }).delete(range).run();
+      }
       // Remove trigger text for command menu
-      commands.delete(range);
       return true;
     },
-    [commands, range]
+    [chain, range]
   );
 
-  const items = ["do-something"];
+  const items = [
+    { name: "H1", type: "heading", level: 1 },
+    { name: "H2", type: "heading", level: 2 },
+    { name: "H3", type: "heading", level: 3 },
+    { name: "H4", type: "heading", level: 4 },
+    { name: "H5", type: "heading", level: 5 },
+    { name: "H6", type: "heading", level: 6 },
+  ];
 
-  const { getMenuProps, getItemProps } = useMenuNavigation({
+  const { getMenuProps, getItemProps, indexIsSelected } = useMenuNavigation({
     items,
     isOpen: enabled,
     onSubmit,
+    direction: "vertical",
     onDismiss: useCallback(() => true, []),
   });
 
   return (
     <FloatingWrapper
-      positioner="cursor"
+      positioner="always"
       enabled={enabled}
-      placement="bottom-start"
+      placement="auto"
+      renderOutsideEditor
     >
-      <div {...getMenuProps()}>
-        {items.map((item, index) => (
-          <li key={item} {...getItemProps({ item, index })}>
-            {item}
-          </li>
-        ))}
-      </div>
+      <ul
+        className={`${
+          !enabled ? "hidden" : ""
+        } remirror-mention-atom-popup-wrapper z-5`}
+        {...getMenuProps()}
+      >
+        {items.map((item, index) => {
+          return (
+            <li
+              className={`remirror-mention-atom-popup-item ${
+                indexIsSelected(index)
+                  ? "remirror-mention-atom-popup-highlight"
+                  : ""
+              }`}
+              key={item.name}
+              {...getItemProps({ item, index })}
+            >
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
     </FloatingWrapper>
   );
 }
