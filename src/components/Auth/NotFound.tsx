b3517b1ea79c9e914ae5ae687babde442cffca10
Arctur <mihailo.dilparic@gmail.com>
2022-09-08 13:34:46 +0200

    Update NotFound.tsx

--- src/components/Auth/NotFound.tsx
+++ src/components/Auth/NotFound.tsx
@@ -5,6 +5,7 @@ import { toastError } from "../../utils/utils";
 type Props = {};
 
 export default function NotFound({ }: Props) {
+  console.log(window.location.href)
   toastError("Route not found!");
   return null;
 }
