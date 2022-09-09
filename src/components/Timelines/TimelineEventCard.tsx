00443933c4b265224377ab2417766d8131e30c22
Arctur <mihailo.dilparic@gmail.com>
2022-09-09 14:39:49 +0200

    Added command bar to timeline view

--- src/components/Timelines/TimelineEventCard.tsx
+++ src/components/Timelines/TimelineEventCard.tsx
@@ -1,12 +1,12 @@
 import { Button } from 'primereact/button'
 import { Card } from 'primereact/card'
-import { TimelineEventType } from '../../types/TimelineTypes'
+import { TimelineEventType } from '../../types/TimelineEventTypes'
 import { supabaseStorageImagesLink } from '../../utils/utils'
 
 
-export default function TimelineEventCard({ title, image }: TimelineEventType) {
+export default function TimelineEventCard({ title, image, start_day, start_month, start_year, end_day, end_month, end_year }: TimelineEventType) {
     return (
-        <Card title={title} className="w-20rem">
+        <Card title={title} subTitle={`${start_day || ""}/${start_month || ""}/${start_year} - ${end_day || ""}/${end_month || ""}/${end_year}`} className="w-20rem h-20rem">
             {image && <img src={`${supabaseStorageImagesLink}/${image.link}`} alt={title} width={200} className="shadow-1" />}
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
                 quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
--- src/components/Timelines/TimelineView.tsx
+++ src/components/Timelines/TimelineView.tsx
@@ -26,14 +26,31 @@ export default function TimelineView({ public_view }: Props) {
     return (
         <div className={`${public_view ? "w-full" : "w-10"
             } h-full flex align-items-end justify-content-center`}>
+            <div
+                className="w-2 absolute border-round surface-50 text-white h-3rem flex align-items-center justify-content-around shadow-5"
+                style={{
+                    top: "95.6vh",
+                    left: "50%",
+                    zIndex: 5,
+                }}
+            >
+                <Button className="p-button-text w-3rem h-3rem" icon="pi pi-plus" tooltip="New Event" tooltipOptions={{ position: "top" }}
+                    onClick={() => setShowTimelineEventDialog(true)}
+                />
+                <Button className="p-button-text w-3rem h-3rem" icon="pi pi-id-card" tooltip="New Event" tooltipOptions={{ position: "top" }}
+                />
+                <Button className="p-button-text w-3rem h-3rem" icon="pi pi-circle-fill" tooltip="New Event" tooltipOptions={{ position: "top" }}
+                />
+                <Button className="p-button-text w-3rem h-3rem" icon="pi pi-arrows-h" tooltip="New Event" tooltipOptions={{ position: "top" }}
+                />
+                <Button className="p-button-text w-3rem h-3rem" icon="pi pi-arrows-v" tooltip="New Event" tooltipOptions={{ position: "top" }}
+                />
+            </div>
             <TimelineEventDialog showDialog={showTimelineEventDialog} setShowDialog={setShowTimelineEventDialog} />
             {timelineData &&
                 <div className="w-full h-full flex px-4 align-items-center overflow-x-auto">
+                    <Timeline className="h-10rem w-full" value={timelineData?.timeline_events || []} content={(item: TimelineEventType) => <TimelineEventCard {...item} />} align="alternate" layout="horizontal" />
 
-                    <Timeline className="h-10rem" value={timelineData?.timeline_events || []} content={(item: TimelineEventType) => <TimelineEventCard {...item} />} align="alternate" layout="horizontal" />
-                    <Button className="p-button-rounded w-3rem h-3rem" icon="pi pi-plus" tooltip="New Event" tooltipOptions={{ position: "left" }}
-                        onClick={() => setShowTimelineEventDialog(true)}
-                    />
                 </div>
             }
         </div>
