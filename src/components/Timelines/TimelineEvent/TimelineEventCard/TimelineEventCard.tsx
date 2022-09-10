2b2020c6fa0cf516dfaa5bc627725c5d860aab9c
Arctur <mihailo.dilparic@gmail.com>
2022-09-10 10:40:33 +0200

    Updated styling for event cards

--- src/components/Timelines/TimelineEvent/TimelineEventCard/TimelineEventCard.tsx
+++ src/components/Timelines/TimelineEvent/TimelineEventCard/TimelineEventCard.tsx
@@ -1,34 +1,37 @@
 import { Card } from "primereact/card";
+import { useContext } from "react";
 import { TimelineEventType } from "../../../../types/TimelineEventTypes";
 import { supabaseStorageImagesLink } from "../../../../utils/utils";
+import { TimelineEventContext } from "../../../Context/TimelineEventContext";
 import TimelineEventCardSubtitle from "./TimelineEventCardSubtitle";
-import TimelineEventCardTitle from "./TimelineEventCardTitle";
 
 type Props = {
     eventData: TimelineEventType
 }
 export default function TimelineEventCard({ eventData }: Props) {
-    const { title, start_day, start_month, start_year, end_day, end_month, end_year, image } = eventData
+    const { title, start_day, start_month, start_year, end_day, end_month, end_year, image } = eventData;
+    const { setShowUpdateDialog, setEventData } = useContext(TimelineEventContext);
     return (
-        <Card
-            title={() => <TimelineEventCardTitle title={title} />}
-            subTitle={() => <TimelineEventCardSubtitle start_day={start_day} start_month={start_month} start_year={start_year} end_day={end_day} end_month={end_month} end_year={end_year} />}
-            className="w-20rem h-20rem timelineEventCard"
-        >
-            {image && (
-                <img
-                    src={`${supabaseStorageImagesLink}/${image.link}`}
-                    alt={title}
-                    width={200}
-                    className="shadow-1"
-                />
-            )}
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
-                consequuntur error repudiandae numquam deserunt quisquam repellat libero
-                asperiores earum nam nobis, culpa ratione quam perferendis esse,
-                cupiditate neque quas!
-            </p>
-        </Card>
+        <div className="h-20rem flex timelineEventCardContainer">
+
+            <Card
+                title={() => <div className="timelineCardTitle text-center">{title} <i className="pi pi-pencil" onClick={() => {
+                    setEventData(eventData);
+                    setShowUpdateDialog(true)
+                }}></i></div>}
+                subTitle={() => <TimelineEventCardSubtitle start_day={start_day} start_month={start_month} start_year={start_year} end_day={end_day} end_month={end_month} end_year={end_year} />}
+                className="w-20rem h-min timelineEventCard"
+            >
+                {image && (
+                    <img
+                        src={`${supabaseStorageImagesLink}/${image.link}`}
+                        alt={title}
+                        width={200}
+                        className="shadow-1"
+                    />
+                )}
+
+            </Card>
+        </div>
     );
 }
--- src/components/Timelines/TimelineEvent/TimelineEventCard/TimelineEventCardTitle.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-    title: string;
-}
-
-export default function TimelineEventCardTitle({ title }: Props) {
-    return (
-        <div className="timelineCardTitle text-center">{title} <i className="pi pi-pencil"></i></div>
-    )
-}
