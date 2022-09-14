944014540c7f9f359e2638176c7bf4fc3a518055
Arctur <mihailo.dilparic@gmail.com>
2022-09-14 19:58:23 +0200

    Added search for public pages in public wiki

--- src/components/PublicView/Wiki/PublicWiki.tsx
+++ src/components/PublicView/Wiki/PublicWiki.tsx
@@ -18,10 +18,11 @@ export default function PublicWiki() {
     doc_id as string,
     async () => await getSingleDocument(doc_id as string)
   );
-  const { data: publicDocuments, isLoading: isLoadingPublicDocuments } = useQuery(
-    `publicDocuments-${project_id}`,
-    async () => await getPublicDocuments(project_id as string)
-  );
+  const { data: publicDocuments, isLoading: isLoadingPublicDocuments } =
+    useQuery(
+      `publicDocuments-${project_id}`,
+      async () => await getPublicDocuments(project_id as string)
+    );
   const [search, setSearch] = useState<any>();
   const [filtered, setFiltered] = useState<any>([]);
   const navigate = useNavigate();
@@ -31,22 +32,37 @@ export default function PublicWiki() {
     return <Navigate to={-1 as To} />;
   }
   return (
-    <div className="w-full h-full flex flex-wrap overflow-y-auto">
+    <div className="w-full h-full flex flex-column overflow-y-auto py-6">
+      <div className="w-full lg:w-6 mx-auto flex justify-content-end align-items-end">
+        <AutoComplete
+          value={search}
+          suggestions={filtered}
+          completeMethod={(e) => {
+            let _filteredDocs;
+            if (!e.query.trim().length && publicDocuments) {
+              _filteredDocs = [...publicDocuments];
+            } else {
+              _filteredDocs = publicDocuments?.filter((doc) => {
+                return doc.title.toLowerCase().includes(e.query.toLowerCase());
+              });
+            }
+            setFiltered(_filteredDocs);
+          }}
+          className="h-2rem w-full flex justify-content-end"
+          inputClassName="border-x-none border-top-none border-bottom-2 w-15rem mb-1"
+          placeholder="🔍"
+          field="title"
+          onChange={(e) => setSearch(e.value)}
+          onSelect={(e) => {
+            if (e.value) {
+              navigate(`../wiki/${e.value.id}`);
+              setSearch(null)
+            }
+          }}
+        />
+      </div>
 
-      <AutoComplete value={search} suggestions={filtered} completeMethod={(e) => {
-        let _filteredDocs;
-        if (!e.query.trim().length && publicDocuments) {
-          _filteredDocs = [...publicDocuments];
-        }
-        else {
-          _filteredDocs = publicDocuments?.filter((doc) => {
-            return doc.title.toLowerCase().includes(e.query.toLowerCase());
-          });
-        }
-        setFiltered(_filteredDocs);
-      }} className="w-full" field="title" inputClassName="w-full" onChange={(e) => setSearch(e.value)} onSelect={(e) => { if (e.value) navigate(`../wiki/${e.value.id}`) }} />
-
-      <div className="w-full lg:w-8 mx-auto h-fit surface-50 shadow-3 mt-6">
+      <div className="w-full lg:w-6 mx-auto h-fit surface-50 shadow-3">
         <h1 className="Merriweather text-white text-center">
           {document?.title || ""}
         </h1>
