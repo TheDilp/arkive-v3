8528c85796095cb7b414a68656299e8384fe1450
Arctur <mihailo.dilparic@gmail.com>
2022-11-12 14:06:29 +0100

    Add bottom border for tabs

--- src/components/Tab/Tabs.tsx
+++ src/components/Tab/Tabs.tsx
@@ -6,16 +6,14 @@ type Props = {
 
 export default function TabsList({ tabs }: Props) {
   return (
-    <Tab.List className="flex">
+    <Tab.List className="flex border-b border-gray-700">
       {tabs.map((tab) => (
         <Tab key={tab} className="flex flex-1 border-none outline-none">
           {({ selected }) => (
-            /* Use the `selected` state to conditionally style the selected tab. */
             <div
               className={`flex-1 px-4 py-2
             ${selected ? "bg-blue-500 text-white" : ""}
-          `}
-            >
+          `}>
               {tab}
             </div>
           )}
