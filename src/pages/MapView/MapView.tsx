8f29abfe3c2c9413f732e98da6d5d7b0b0867ae0
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2022-12-04 09:36:42 +0100

    Actually fix map layers

--- src/pages/MapView/MapView.tsx
+++ src/pages/MapView/MapView.tsx
@@ -1,14 +1,15 @@
-import { useQueryClient } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 import { CRS, LatLngBoundsExpression } from "leaflet";
+import { ProgressSpinner } from "primereact/progressspinner";
 import { useEffect, useRef, useState } from "react";
 import { ImageOverlay, MapContainer } from "react-leaflet";
 import { useParams } from "react-router-dom";
 
 import ContextMenu from "../../components/ContextMenu/ContextMenu";
 import MapImage from "../../components/Map/MapImage";
+import { useGetItem } from "../../hooks/getItemHook";
 import { baseURLS, getURLS } from "../../types/CRUDenums";
-import { MapType } from "../../types/mapTypes";
+import { MapPinType, MapType } from "../../types/mapTypes";
 import { DrawerAtom } from "../../utils/Atoms/atoms";
 import { DefaultDrawer } from "../../utils/DefaultValues/DrawerDialogDefaults";
 
@@ -17,13 +18,14 @@ type Props = {
 };
 
 export default function MapView({ isReadOnly }: Props) {
-  const queryClient = useQueryClient();
-  const { project_id, item_id } = useParams();
+  const { project_id, item_id, pin_id } = useParams();
   const [bounds, setBounds] = useState<number[][]>([
     [0, 0],
     [0, 0],
   ]);
+  const [loading, setLoading] = useState(true);
   const [drawer, setDrawer] = useAtom(DrawerAtom);
+
   const mapRef = useRef() as any;
   const imgRef = useRef() as any;
   const cm = useRef() as any;
@@ -40,8 +42,7 @@ export default function MapView({ isReadOnly }: Props) {
       label: "Fit Map",
     },
   ];
-
-  const currentMap = queryClient.getQueryData<MapType[]>(["allItems", project_id, "maps"])?.find((map) => map.id === item_id);
+  const currentMap = useGetItem(project_id as string, item_id as string, "maps") as MapType;
   useEffect(() => {
     if (currentMap) {
       const img = new Image();
@@ -52,61 +53,65 @@ export default function MapView({ isReadOnly }: Props) {
           [img.height, img.width],
         ]);
         if (imgRef.current) {
-          imgRef.current.setBounds([
-            [0, 0],
-            [img.height, img.width],
-          ]);
-          mapRef.current.fitBounds(
-            [
+          setTimeout(() => {
+            imgRef.current.setBounds([
               [0, 0],
               [img.height, img.width],
-            ],
-            {
-              animate: false,
-            },
-          );
+            ]);
+            mapRef.current.fitBounds(
+              [
+                [0, 0],
+                [img.height, img.width],
+              ],
+              {
+                animate: false,
+              },
+            );
+          }, 250);
         }
+        setLoading(false);
       };
     }
   }, [currentMap, project_id]);
-  useEffect(() => {
-    //  Wait for map to finish loading
-  }, [bounds]);
+
+  if (loading) return <ProgressSpinner />;
   return (
-    <div className="flex flex-col flex-1 w-full h-full">
-      <ContextMenu cm={cm} items={items} />
+    <div className="flex flex-col w-full h-full">
       <link href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" rel="stylesheet" />
+      <ContextMenu cm={cm} items={items} />
       {/* This div is needed for layers to properly work */}
-      <div className="w-full h-full">
-        <MapContainer
-          ref={mapRef}
-          attributionControl={false}
-          bounds={bounds as LatLngBoundsExpression}
-          center={[bounds[1][0] / 2, bounds[1][1] / 2]}
-          className="flex-1 w-full h-full outline-none bg-zinc-900"
-          crs={CRS.Simple}
-          maxZoom={2}
-          minZoom={-3}
-          scrollWheelZoom
-          zoom={0}
-          zoomSnap={0}>
-          <MapImage
+      {currentMap ? (
+        <div className="w-full h-full">
+          <MapContainer
+            ref={mapRef}
+            attributionControl={false}
             bounds={bounds as LatLngBoundsExpression}
-            cm={cm}
-            imgRef={imgRef}
-            isReadOnly={isReadOnly}
-            src={`${baseURLS.baseServer}${getURLS.getSingleMapImage}${project_id}/${currentMap?.map_image}`}
-          />
-          <ImageOverlay
-            ref={imgRef}
-            bounds={[
-              [0, 0],
-              [0, 0],
-            ]}
-            url={`${baseURLS.baseServer}${getURLS.getSingleMapImage}${project_id}/${currentMap?.map_image}`}
-          />
-        </MapContainer>
-      </div>
+            center={[bounds[1][0] / 2, bounds[1][1] / 2]}
+            className="flex-1 w-full h-full outline-none bg-zinc-900"
+            crs={CRS.Simple}
+            maxZoom={2}
+            minZoom={-3}
+            scrollWheelZoom
+            zoom={0}
+            zoomSnap={0}>
+            <MapImage
+              bounds={bounds as LatLngBoundsExpression}
+              cm={cm}
+              imgRef={imgRef}
+              isReadOnly={isReadOnly}
+              src={`${baseURLS.baseServer}${getURLS.getSingleMapImage}${project_id}/${currentMap?.map_image}`}
+            />
+            <ImageOverlay
+              ref={imgRef}
+              bounds={[
+                [0, 0],
+                [0, 0],
+              ]}
+              url={`${baseURLS.baseServer}${getURLS.getSingleMapImage}${project_id}/${currentMap?.map_image}`}
+            />
+          </MapContainer>
+        </div>
+      ) : null}
     </div>
   );
 }
