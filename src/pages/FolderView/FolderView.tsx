83835a89490cb3eec173f10b0fc73b65d48a4f1f
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2022-12-10 15:05:18 +0100

    Remove table folder view

--- src/pages/FolderView/FolderView.tsx
+++ src/pages/FolderView/FolderView.tsx
@@ -1,4 +1,3 @@
-import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { ToggleButton } from "primereact/togglebutton";
 import { useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
@@ -8,50 +7,23 @@ import { useGetAllItems } from "../../CRUD/ItemsCRUD";
 import { AllItemsType, AvailableItemTypes } from "../../types/generalTypes";
 import { getIcon, getType } from "../../utils/transform";
 
-const columnHelper = createColumnHelper<AllItemsType>();
-
-const columns = [
-  // Display Column
-  columnHelper.display({
-    id: "actions",
-    cell: () => "TEST",
-  }),
-  // Grouping Column
-  columnHelper.group({
-    header: "Name",
-    footer: (props) => props.column.id,
-    columns: [
-      // Accessor Column
-      columnHelper.accessor("title", {
-        cell: (info) => info.getValue(),
-        footer: (props) => props.column.id,
-      }),
-    ],
-  }),
-];
-
 function FolderViewCards({ type, items }: { type: AvailableItemTypes; items: AllItemsType[] }) {
   return (
-    <>
+    <div className="flex flex-wrap">
       {items.map((item: AllItemsType) => (
         <FolderCard key={item.id} icon={getIcon(type, item)} id={item.id} isFolder={item.folder} title={item.title} />
       ))}
-    </>
+    </div>
   );
 }
 
 export default function FolderView() {
   const { project_id, item_id } = useParams();
   const { pathname } = useLocation();
-  const [view, setView] = useState(true);
+  const [view, setView] = useState(false);
   const type = getType(pathname);
   const { data, isLoading, isError } = useGetAllItems(project_id as string, type);
 
-  const table = useReactTable({
-    data: data || [],
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
   if (isLoading || isError) return null;
 
   const currentItems = data.filter((item) => {
@@ -64,30 +36,7 @@ export default function FolderView() {
       <div className="w-full">
         <ToggleButton checked={view} offLabel="Table View" onChange={(e) => setView(e.value)} onLabel="Card View" />
       </div>
-      <div className="flex flex-wrap">
-        {view ? (
-          <FolderViewCards items={currentItems} type={type} />
-        ) : (
-          <div>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <div key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <div key={header.id}>
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </div>
-                ))}
-              </div>
-            ))}
-            {table.getRowModel().rows.map((row) => (
-              <div key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <div key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</div>
-                ))}
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      <FolderViewCards items={currentItems} type={type} />
     </div>
   );
 }
