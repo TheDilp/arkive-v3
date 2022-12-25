85ac69d70dd747d614a3e66f733d3608962bcb8c
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2022-12-25 11:23:12 +0100

    Fix editors in doc settings

--- src/pages/Settings/DocumentSettings.tsx
+++ src/pages/Settings/DocumentSettings.tsx
@@ -1,9 +1,10 @@
 import { Icon } from "@iconify/react";
-import { UseMutationResult, useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 import { Checkbox } from "primereact/checkbox";
 import { Column, ColumnEditorOptions } from "primereact/column";
 import { DataTable } from "primereact/datatable";
 import { Dropdown } from "primereact/dropdown";
+import { InputText } from "primereact/inputtext";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -12,7 +13,6 @@ import ImageDropdownValue from "../../components/Dropdown/ImageDropdownValue";
 import { IconSelect } from "../../components/IconSelect/IconSelect";
 import { useGetAllImages, useGetAllItems, useUpdateItem } from "../../CRUD/ItemsCRUD";
 import { DocumentType } from "../../types/documentTypes";
-import { AllItemsType } from "../../types/generalTypes";
 import { getImageLink } from "../../utils/CRUD/CRUDUrls";
 import { toaster } from "../../utils/toast";
 
@@ -25,7 +25,23 @@ function getCheckedValue(
   if (type === "isPublic") return isPublic;
   return false;
 }
+function TitleEditor(editorOptions: ColumnEditorOptions, updateDocument: any) {
+  const { rowData, editorCallback } = editorOptions;
 
+  return (
+    <InputText
+      onChange={(e) => {
+        if (rowData.id && e.currentTarget.value && editorCallback) editorCallback(e.target.value);
+      }}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          updateDocument({ id: rowData.id, title: e.currentTarget.value });
+        }
+      }}
+      value={rowData.title}
+    />
+  );
+}
 function IconColumn({ id, icon, folder }: DocumentType) {
   const { project_id } = useParams();
   const queryClient = useQueryClient();
@@ -33,6 +49,7 @@ function IconColumn({ id, icon, folder }: DocumentType) {
   return (
     <div className="flex justify-center">
       <IconSelect
+        disabled={folder}
         setIcon={(newIcon) => {
           updateDocumentMutation?.mutate(
             { icon: newIcon, id },
@@ -45,7 +62,7 @@ function IconColumn({ id, icon, folder }: DocumentType) {
           );
         }}>
         <Icon
-          className="cursor-pointer rounded-full hover:bg-sky-400"
+          className={`rounded-full ${folder ? "" : "cursor-pointer hover:bg-sky-400"}`}
           fontSize={24}
           icon={folder ? "mdi:folder" : icon || "mdi:file"}
           inline
@@ -62,37 +79,14 @@ function ImageColumn({ image }: DocumentType) {
     </div>
   ) : null;
 }
-function ImageEditor(
-  editorOptions: ColumnEditorOptions,
-  images: string[] | undefined,
-  updateDocumentMutation: UseMutationResult<
-    Response | null,
-    unknown,
-    Partial<AllItemsType>,
-    {
-      oldData: unknown;
-    }
-  >,
-) {
-  const { project_id } = useParams();
-  const queryClient = useQueryClient();
+function ImageEditor(editorOptions: ColumnEditorOptions, images: string[] | undefined, updateDocument: any) {
   const { rowData } = editorOptions as { rowData: DocumentType };
   return (
     <div className="w-36">
       <Dropdown
         className="w-full"
         itemTemplate={ImageDropdownItem}
-        onChange={(e) =>
-          updateDocumentMutation.mutate(
-            { id: rowData.id, image: e.value === "None" ? undefined : e.value },
-            {
-              onSuccess: () => {
-                queryClient.refetchQueries({ queryKey: ["allItems", project_id, "documents"] });
-                toaster("success", "Icon updated successfully.");
-              },
-            },
-          )
-        }
+        onChange={(e) => updateDocument({ id: rowData.id, image: e.value })}
         options={["None", ...(images || [])] || []}
         placeholder="Select map"
         value={rowData?.image}
@@ -131,7 +125,7 @@ function FolderTemplatePublicColumn(
 function ParentColumn({ parent }: DocumentType, documents: DocumentType[]) {
   // eslint-disable-next-line react/destructuring-assignment
   const parentFolder = documents?.find((doc) => doc.id === parent);
-  if (parentFolder) return <div className="w-24">{parentFolder.title}</div>;
+  if (parentFolder) return <div className="w-full">{parentFolder.title}</div>;
   return null;
 }
 
@@ -139,31 +133,46 @@ export default function DocumentSettings() {
   const { project_id } = useParams();
 
   const [selected, setSelected] = useState([]);
-  const updateDocumentMutation = useUpdateItem("documents");
+  const { mutate } = useUpdateItem("documents");
+  const queryClient = useQueryClient();
+
+  const updateDocument = (data: Partial<DocumentType>) =>
+    mutate(data, {
+      onSuccess: () => {
+        queryClient.refetchQueries({ queryKey: ["allItems", project_id, "documents"] });
+        toaster("success", "Item updated successfully.");
+      },
+    });
 
   const { data: documents } = useGetAllItems(project_id as string, "documents") as { data: DocumentType[] };
   const { data: images } = useGetAllImages(project_id as string);
 
   return (
-    <div>
+    <div className="p-4">
       <DataTable
         dataKey="id"
+        editMode="cell"
+        filterDisplay="menu"
         onSelectionChange={(e) => setSelected(e.value)}
+        removableSort
         selection={selected}
+        selectionMode="checkbox"
         showGridlines
+        size="small"
+        sortMode="multiple"
         value={documents}>
         <Column headerClassName="w-12" selectionMode="multiple" />
-        <Column field="title" header="Title" />
+        <Column editor={(e) => TitleEditor(e, updateDocument)} field="title" header="Title" />
         <Column align="center" body={IconColumn} className="w-24" field="icon" header="Icon" />
         <Column
           align="center"
           body={ImageColumn}
           className="w-36"
-          editor={(e) => ImageEditor(e, images, updateDocumentMutation)}
+          editor={(e) => ImageEditor(e, images, updateDocument)}
           field="image"
           header="Image"
         />
-        <Column align="center" body={(data) => ParentColumn(data, documents)} className="w-10" field="parent" header="Parent" />
+        <Column align="center" body={(data) => ParentColumn(data, documents)} className="w-48" field="parent" header="Parent" />
         <Column
           align="center"
           body={(data) => FolderTemplatePublicColumn(data, "folder")}
