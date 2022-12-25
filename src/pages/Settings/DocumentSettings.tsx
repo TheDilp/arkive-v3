723a73e090f760a0e45ab89255eb3440f47c0e5f
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2022-12-25 09:18:54 +0100

    Change DocumentSettings to prime table

--- src/pages/Settings/DocumentSettings.tsx
+++ src/pages/Settings/DocumentSettings.tsx
@@ -1,125 +1,49 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { Icon } from "@iconify/react";
-import {
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  SortingState,
-  useReactTable,
-} from "@tanstack/react-table";
-import { Checkbox } from "primereact/checkbox";
-import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
 import { useParams } from "react-router-dom";
 
-import { useGetAllItems } from "../../CRUD/ItemsCRUD";
+import { IconSelect } from "../../components/IconSelect/IconSelect";
+import { useGetAllItems, useUpdateItem } from "../../CRUD/ItemsCRUD";
 import { DocumentType } from "../../types/documentTypes";
+import { toaster } from "../../utils/toast";
 
-const columnHelper = createColumnHelper<Omit<DocumentType, "project_id" | "content">>();
+type Props = {};
 
-const columns = [
-  columnHelper.display({
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        {...{
-          checked: table.getIsAllRowsSelected(),
-          onChange: table.getToggleAllRowsSelectedHandler(),
-        }}
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        {...{
-          checked: row.getIsSelected(),
-          onChange: row.getToggleSelectedHandler(),
-        }}
-      />
-    ),
-  }),
-  columnHelper.accessor("title", {
-    header: "Title",
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor("image", {
-    header: "Image",
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor("icon", {
-    header: () => <div className="flex w-full justify-center">Icon</div>,
-    cell: ({ getValue, row }) => (
-      <div className="flex w-full justify-center">
-        <Icon fontSize={24} icon={getValue() || (row.original.folder ? "mdi:folder" : "mdi:file")} />
-      </div>
-    ),
-  }),
-];
-function getHeaderClassNames(select: boolean, canSort: boolean) {
-  if (select) return "w-8";
-  if (canSort) return "cursor-pointer select-none flex flex-1 truncate items-start px-2";
-  return "flex flex-1 truncate items-start px-2";
-}
-function getRowClassNames(select: boolean) {
-  if (select) return "w-8";
-  return "flex flex-1 items-center truncate px-2";
-}
-export default function DocumentSettings() {
+function IconColumn({ id, icon }: DocumentType) {
   const { project_id } = useParams();
-  const { data } = useGetAllItems(project_id as string, "documents") as { data: DocumentType[] };
-  const [rowSelection, setRowSelection] = useState({});
-  const [sorting, setSorting] = useState<SortingState>([]);
-  const table = useReactTable({
-    data:
-      data.filter((item) => {
-        if ("folder" in item && item.folder) return false;
-        if ("template" in item && item.template) return false;
-        return true;
-      }) || [],
-    state: {
-      rowSelection,
-      sorting,
-    },
-    columns,
-    onSortingChange: setSorting,
-    getCoreRowModel: getCoreRowModel(),
-    onRowSelectionChange: setRowSelection,
-    getSortedRowModel: getSortedRowModel(),
-  });
+  const queryClient = useQueryClient();
+  const updateDocumentMutation = useUpdateItem("documents");
   return (
-    <div className="h-full w-full">
-      <div className="flex h-full w-full flex-col border border-zinc-700">
-        <div className="flex items-center px-4 py-2">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <div key={headerGroup.id} className="flex flex-1">
-              {headerGroup.headers.map((header) => (
-                <div
-                  key={header.id}
-                  {...{
-                    className: getHeaderClassNames(header.id === "select", header.column.getCanSort()),
-                    onClick: header.column.getToggleSortingHandler(),
-                  }}>
-                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  {{
-                    asc: " 🔼",
-                    desc: " 🔽",
-                  }[header.column.getIsSorted() as string] ?? null}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-        <div className="flex h-full w-full flex-col">
-          {table.getRowModel().rows.map((row) => (
-            <div key={row.id} className="flex w-full items-center  border-zinc-700 px-4 py-2 last:border-b odd:border-y">
-              {row.getVisibleCells().map((cell) => (
-                <div key={cell.id} className={getRowClassNames(cell.column.id === "select")}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      </div>
+    <IconSelect
+      setIcon={(newIcon) => {
+        updateDocumentMutation?.mutate(
+          { icon: newIcon, id },
+          {
+            onSuccess: () => {
+              queryClient.refetchQueries({ queryKey: ["allItems", project_id, "documents"] });
+              toaster("success", "Icon updated successfully.");
+            },
+          },
+        );
+      }}>
+      <Icon className="cursor-pointer rounded-full hover:bg-sky-400" icon={icon || "mdi:file"} inline fontSize={24} />
+    </IconSelect>
+  );
+}
+
+export default function DocumentSettings({}: Props) {
+  const { project_id } = useParams();
+  const { data: documents } = useGetAllItems(project_id as string, "documents") as { data: DocumentType[] };
+
+  return (
+    <div>
+      <DataTable responsiveLayout="scroll" value={documents.filter((doc) => !doc.folder && !doc.template)}>
+        <Column field="title" header="title" />
+        <Column body={IconColumn} field="icon" header="Icon" />
+        <Column field="image" header="Image" />
+      </DataTable>
     </div>
   );
 }
