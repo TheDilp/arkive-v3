01e30ea628ba937aa568cc2b6ecaffc554b752db
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-01-19 07:48:13 +0100

    Fix image links in dropdown items

--- src/components/Dropdown/ImageDropdownItem.tsx
+++ src/components/Dropdown/ImageDropdownItem.tsx
@@ -1,18 +1,15 @@
 import { useParams } from "react-router-dom";
 
-import { getMapImageLink } from "../../utils/CRUD/CRUDUrls";
-
 type Props = string;
 
 export function MapImageDropdownItem(props: Props) {
   const { project_id } = useParams();
   const image = props;
+  const name = image.split("/").pop();
   return (
     <div className="flex max-w-[200px] items-center justify-between">
-      <span className="truncate">{image}</span>
-      {image !== "None" ? (
-        <img alt={project_id} className="h-12 w-12" src={getMapImageLink(image, project_id as string)} />
-      ) : null}
+      <span className="truncate">{name}</span>
+      {image !== "None" ? <img alt={project_id} className="h-12 w-12" src={image} /> : null}
     </div>
   );
 }
@@ -20,9 +17,10 @@ export function MapImageDropdownItem(props: Props) {
 export function ImageDropdownItem(props: Props) {
   const { project_id } = useParams();
   const image = props;
+  const name = image.split("/").pop();
   return (
     <div className="flex max-w-[200px] items-center justify-between">
-      <span className="truncate">{image}</span>
+      <span className="truncate">{name}</span>
       {image !== "None" ? <img alt={project_id} className="h-12 w-12" src={image} /> : null}
     </div>
   );
