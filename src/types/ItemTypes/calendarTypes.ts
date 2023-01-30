b912518e2cbd0f56d6a9e7bebbfc71a192696b66
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-01-30 18:28:57 +0100

    Update calendarTypes

--- src/types/ItemTypes/calendarTypes.ts
+++ src/types/ItemTypes/calendarTypes.ts
@@ -1,10 +1,54 @@
+/* eslint-disable no-use-before-define */
 import { BaseItemType } from "../generalTypes";
 
 export interface CalendarType extends BaseItemType {
+  id: string;
+  title: string;
+
+  eras: EraType[];
+  months: MonthType[];
   days: string[];
-  weeks: number;
-  months: string[];
+  event: EventType[];
 }
+
+export type EraType = {
+  id: string;
+  title: string;
+  start_year: number;
+  end_year: number;
+  hours: number;
+  minutes: number;
+  parentId: string;
+  sort: number;
+
+  events: EventType[];
+};
+
+export type MonthType = {
+  id: string;
+  title: string;
+  days: number;
+  sort: number;
+  parentId: string;
+
+  events: EventType[];
+};
+
+export type EventType = {
+  id: string;
+  title: string;
+  year: number;
+
+  era: EraType;
+  month: MonthType;
+  day: number;
+  hours: number;
+  minutes: number;
+
+  erasId: string;
+  monthsId: string;
+  calendarsId: string | null;
+};
 
 export type CalendarCreateType = Partial<Omit<CalendarType, "parentId">>;
 
