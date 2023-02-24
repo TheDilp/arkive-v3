f7a0d86a5bcf56386f3413a07877abc36367fa0c
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-02-24 08:56:37 +0100

    Hide user invites

--- src/pages/Settings/ProjectSettings.tsx
+++ src/pages/Settings/ProjectSettings.tsx
@@ -1,5 +1,4 @@
 import { useAtom } from "jotai";
-import { Avatar } from "primereact/avatar";
 import { Button } from "primereact/button";
 import { Dropdown } from "primereact/dropdown";
 import { InputText } from "primereact/inputtext";
@@ -8,7 +7,6 @@ import { useEffect, useState } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 
 import defaultImage from "../../assets/DefaultProjectImage.jpg";
-import DrawerSection from "../../components/Drawer/DrawerSection";
 import { ImageDropdownItem } from "../../components/Dropdown/ImageDropdownItem";
 import ImageDropdownValue from "../../components/Dropdown/ImageDropdownValue";
 import { useGetAllImages } from "../../CRUD/ItemsCRUD";
@@ -17,8 +15,6 @@ import { baseURLS } from "../../types/CRUDenums";
 import { ProjectType } from "../../types/ItemTypes/projectTypes";
 import { UserAtom } from "../../utils/Atoms/atoms";
 import { deleteItem } from "../../utils/Confirms/Confirm";
-import { FetchFunction } from "../../utils/CRUD/CRUDFetch";
-import { userPermissions } from "../../utils/settingsUtils";
 import { toaster } from "../../utils/toast";
 import { virtualScrollerSettings } from "../../utils/uiUtils";
 
@@ -28,7 +24,6 @@ export default function ProjectSettings() {
   const [userData] = useAtom(UserAtom);
   const { data, isLoading } = useGetSingleProject(project_id as string);
   const { data: allImages } = useGetAllImages(project_id as string);
-  const [userInvite, setUserInvite] = useState({ email: "" });
   const [localItem, setLocalItem] = useState<ProjectType | undefined>(data);
   useEffect(() => {
     if (data) setLocalItem(data);
@@ -89,57 +84,57 @@ export default function ProjectSettings() {
         />
       </div>
 
-      <hr className="border-zinc-700" />
-      <div className="flex flex-col gap-y-4">
-        <h3 className="text-lg font-semibold">Invite to project</h3>
-        <h4 className="text-base font-semibold">Add another user to a project with selected permissions</h4>
-        <div className="flex w-full max-w-lg justify-between gap-x-2">
-          <InputText
-            className="flex-1"
-            name="email"
-            onChange={(e) => setUserInvite((prev) => ({ ...prev, email: e.target.value }))}
-            placeholder="User's email"
-            value={userInvite.email}
-          />
-          <Dropdown
-            onChange={(e) => setUserInvite((prev) => ({ ...prev, permisssion: e.value }))}
-            optionLabel="label"
-            options={userPermissions}
-            optionValue="value"
-            value="editor"
-          />
-          <Button
-            className="p-button-outlined"
-            icon="pi pi-user-plus"
-            iconPos="right"
-            label="Add user"
-            onClick={async () => {
-              FetchFunction({
-                url: `${baseURLS.baseServer}addtoproject`,
-                method: "POST",
-                body: JSON.stringify({ email: userInvite.email, project_id }),
-              });
-              setUserInvite({ email: "" });
-            }}
-          />
-        </div>
-        <div className="flex w-full max-w-lg flex-col">
-          <DrawerSection title="Current Members">
-            {data?.members?.length
-              ? data.members.map((m) => (
-                  <div key={m.user_id} className="flex w-full items-center gap-x-2">
-                    <Avatar
-                      icon="pi pi-user"
-                      image={m.member.image || `https://avatars.dicebear.com/api/bottts/${m.member?.id}.svg`}
-                    />
-                    {m.member.nickname}
-                    <Button className="p-button-danger p-button-text p-button-rounded" icon="pi pi-trash" iconPos="right" />
-                  </div>
-                ))
-              : null}
-          </DrawerSection>
-        </div>
+      {/* <hr className="border-zinc-700" />
+    <div className="flex flex-col gap-y-4">
+      <h3 className="text-lg font-semibold">Invite to project</h3>
+      <h4 className="text-base font-semibold">Add another user to a project with selected permissions</h4>
+      <div className="flex w-full max-w-lg justify-between gap-x-2">
+        <InputText
+          className="flex-1"
+          name="email"
+          onChange={(e) => setUserInvite((prev) => ({ ...prev, email: e.target.value }))}
+          placeholder="User's email"
+          value={userInvite.email}
+        />
+        <Dropdown
+          onChange={(e) => setUserInvite((prev) => ({ ...prev, permisssion: e.value }))}
+          optionLabel="label"
+          options={userPermissions}
+          optionValue="value"
+          value="editor"
+        />
+        <Button
+          className="p-button-outlined"
+          icon="pi pi-user-plus"
+          iconPos="right"
+          label="Add user"
+          onClick={async () => {
+            FetchFunction({
+              url: `${baseURLS.baseServer}addtoproject`,
+              method: "POST",
+              body: JSON.stringify({ email: userInvite.email, project_id }),
+            });
+            setUserInvite({ email: "" });
+          }}
+        />
       </div>
+      <div className="flex w-full max-w-lg flex-col">
+        <DrawerSection title="Current Members">
+          {data?.members?.length
+            ? data.members.map((m) => (
+                <div key={m.user_id} className="flex w-full items-center gap-x-2">
+                  <Avatar
+                    icon="pi pi-user"
+                    image={m.member.image || `https://avatars.dicebear.com/api/bottts/${m.member?.id}.svg`}
+                  />
+                  {m.member.nickname}
+                  <Button className="p-button-danger p-button-text p-button-rounded" icon="pi pi-trash" iconPos="right" />
+                </div>
+              ))
+            : null}
+        </DrawerSection>
+      </div>
+    </div> */}
       <hr className="border-zinc-700" />
 
       <div className="flex flex-col gap-y-2">
