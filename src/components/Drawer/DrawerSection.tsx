ae1a1f07384ef82bcc2671a67f5547e5be801cc8
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-03-06 11:42:17 +0100

    Add subtitle to DrawerSection

--- src/components/Drawer/DrawerSection.tsx
+++ src/components/Drawer/DrawerSection.tsx
@@ -3,12 +3,14 @@ import DrawerSectionTitle from "./DrawerSectionTitle";
 type Props = {
   children: JSX.Element | JSX.Element[] | null;
   title: string;
+  subtitle?: string;
 };
 
-export default function DrawerSection({ children, title }: Props) {
+export default function DrawerSection({ children, title, subtitle }: Props) {
   return (
     <div className="flex flex-col overflow-hidden">
       <DrawerSectionTitle title={title} />
+      {subtitle ? <span className="w-full text-xs text-zinc-400">{subtitle}</span> : null}
       {children}
     </div>
   );
