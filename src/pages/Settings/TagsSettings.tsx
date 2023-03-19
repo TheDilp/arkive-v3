7ab74cfdce39f90c7923d277e2bfdad128d570ce
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-03-19 12:09:17 +0100

    Fix tags

--- src/pages/Settings/TagsSettings.tsx
+++ src/pages/Settings/TagsSettings.tsx
@@ -40,7 +40,7 @@ function ExpandedSection(tag: TagSettingsType) {
   if (!tag) return null;
   const { documents, maps, map_pins, boards, nodes, edges, calendars, events, screens, cards, dictionaries } = tag;
   return (
-    <TabView className="h-[15rem]" panelContainerClassName="h-full" renderActiveOnly>
+    <TabView className="tagSettings h-[15rem]" panelContainerClassName="h-full" renderActiveOnly>
       <TabPanel contentClassName="overflow-hidden h-full" disabled={!documents.length} header="Documents">
         <div className="flex h-full w-full flex-col gap-y-2 overflow-auto px-2">
           {documents.map((doc) => (
