b83cb3be398c7aa4ad96f8c90b9fbeeb58017cfe
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-03-25 11:07:33 +0100

    Clean up asset grid item

--- src/pages/Settings/Assets/GridItem.tsx
+++ src/pages/Settings/Assets/GridItem.tsx
@@ -1,11 +1,6 @@
-import { Button } from "primereact/button";
 import { Image } from "primereact/image";
-import { Tag } from "primereact/tag";
-/* eslint-disable react/destructuring-assignment */
 
 export function ListGridItem(image: string) {
-  const imageName = image.split("/").pop();
-  const isMap = image.includes("maps");
   return (
     <div className="col-span-1 flex flex-col justify-center px-2">
       <Image alt="Listasset" className="h-full w-full" imageClassName="object-contain" preview src={image} />
