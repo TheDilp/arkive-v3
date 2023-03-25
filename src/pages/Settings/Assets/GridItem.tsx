00d5ad6640eb573cb656e86a0a026b27dc7584a0
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-03-25 11:04:50 +0100

    Make grid only images

--- src/pages/Settings/Assets/GridItem.tsx
+++ src/pages/Settings/Assets/GridItem.tsx
@@ -7,15 +7,7 @@ export function ListGridItem(image: string) {
   const imageName = image.split("/").pop();
   const isMap = image.includes("maps");
   return (
-    <div className="col-span-2 flex flex-col justify-center">
-      <div className="flex items-center gap-x-4">
-        <div className="truncate text-xl font-bold">{imageName}</div>
-        <div className="flex items-center gap-x-2">
-          <Tag severity={isMap ? "warning" : "info"} style={{ fontSize: "1rem" }} value={isMap ? "Map" : "Image"} />
-          <Button className="p-button-outlined p-button-danger" icon="pi pi-trash" />
-        </div>
-      </div>
-
+    <div className="col-span-1 flex flex-col justify-center px-2">
       <Image alt="Listasset" className="h-full w-full" imageClassName="object-contain" preview src={image} />
     </div>
   );
