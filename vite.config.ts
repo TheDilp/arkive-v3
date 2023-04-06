6911cb461f1485076d1b14899a34936914c0fdcb
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-04-07 00:41:57 +0200

    Fix Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  define: {
+    global: "globalThis",
+  },
   esbuild: {
     drop: mode === "production" ? ["console", "debugger"] : [],
   },
