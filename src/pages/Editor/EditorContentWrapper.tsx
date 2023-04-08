8292feaad98726c35670c523cf22a07da9f1919b
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-04-08 14:26:19 +0200

    Attempt WS realtime collab

--- src/pages/Editor/EditorContentWrapper.tsx
+++ src/pages/Editor/EditorContentWrapper.tsx
@@ -1,3 +1,4 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { WebsocketProvider } from "y-websocket";
@@ -12,7 +13,7 @@ import EditorContainer from "./EditorContainer";
 export default function EditorContentWrapper() {
   const { item_id } = useParams();
   const firstRender = useRef(true);
-
+  const queryClient = useQueryClient();
   const [provider, setProvider] = useState<WebsocketProvider | null>();
   const { data: currentDocument, isFetching } = useGetItem<DocumentType>(item_id as string, "documents", {
     enabled: !!item_id,
@@ -25,15 +26,18 @@ export default function EditorContentWrapper() {
       setProvider(new WebsocketProvider(import.meta.env.VITE_SYNC_SERVER, item_id as string, yDoc));
       firstRender.current = false;
     } else if (provider) {
-      provider.roomname = item_id as string;
+      provider?.destroy();
+      const yDoc = new Y.Doc();
+      setProvider(new WebsocketProvider(import.meta.env.VITE_SYNC_SERVER, item_id as string, yDoc));
     }
     return () => {
-      // provider?.disconnect();
+      queryClient.removeQueries({ queryKey: ["documents", item_id] });
+      provider?.disconnect();
       // provider?.destroy();
       // setProvider(null);
       firstRender.current = true;
     };
-  }, []);
+  }, [item_id]);
 
   if (isFetching || !currentDocument || !provider) return <LoadingScreen />;
   return (
