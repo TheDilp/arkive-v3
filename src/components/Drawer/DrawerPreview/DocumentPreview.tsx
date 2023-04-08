faaf4926420244d9ddb20b6ab12cc53ba9a39fd1
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-04-08 10:58:48 +0200

    Add title to document preview

--- src/components/Drawer/DrawerPreview/DocumentPreview.tsx
+++ src/components/Drawer/DrawerPreview/DocumentPreview.tsx
@@ -3,6 +3,7 @@ import { RemirrorJSON } from "remirror";
 import { useGetItem } from "../../../hooks/useGetItem";
 import { DocumentType } from "../../../types/ItemTypes/documentTypes";
 import StaticRender from "../../Editor/StaticRender";
+import LoadingScreen from "../../Loading/LoadingScreen";
 
 type Props = {
   id: string;
@@ -11,8 +12,15 @@ type Props = {
 
 export default function DocumentPreview({ id, isReadOnly }: Props) {
   const { data, isFetching } = useGetItem<DocumentType>(id, "documents", { staleTime: 60 * 1000 }, isReadOnly);
-  if (isFetching) return null;
+  if (isFetching) return <LoadingScreen />;
   if (!data) return <span>This document is not public.</span>;
 
-  return <StaticRender content={data.content as RemirrorJSON} isReadOnly={isReadOnly} />;
+  return (
+    <div className="flex h-full flex-col gap-y-2 font-Lato">
+      <h2 className="text-center text-4xl">
+        <div className="flex items-center justify-center">{data?.title}</div>
+      </h2>
+      <StaticRender content={data.content as RemirrorJSON} isReadOnly={isReadOnly} />
+    </div>
+  );
 }
