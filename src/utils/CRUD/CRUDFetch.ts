eb6cf6e14d9428f69ce6ec0886bd04c85bb4049e
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-04-09 20:22:15 +0200

    Change header

--- src/utils/CRUD/CRUDFetch.ts
+++ src/utils/CRUD/CRUDFetch.ts
@@ -10,11 +10,12 @@ export async function FetchFunction({
   body?: string | FormData;
 }) {
   const token = getCookie("__session");
+  const isLocal = import.meta.env.VITE_LOCAL === "true";
   const res = await fetch(url, {
     method,
     body,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": isLocal ? "*" : import.meta.env.VITE_BASE_SERVER,
       ...(typeof body === "string" ? { "Content-Type": "application/json" } : {}),
       Authorization: `Bearer ${token}`,
     },
