1a0b5497ac1e52f440e4ac6644beffafc481d55d
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-05-17 13:20:23 +0200

    fix: timeline view vertical detailed scroll

--- src/pages/TimelineView/TimelineView.tsx
+++ src/pages/TimelineView/TimelineView.tsx
@@ -163,7 +163,11 @@ export default function TimelineView({ isReadOnly }: Props) {
             <div
               className={`
               overlfow-x-auto
-              ${viewSettings.view.value === "Horizontal" ? " flex w-full max-w-full overflow-x-auto" : "w-full max-w-6xl"}`}>
+              ${
+                viewSettings.view.value === "Horizontal"
+                  ? " flex w-full max-w-full overflow-x-auto"
+                  : "w-full max-w-6xl overflow-y-auto"
+              }`}>
               <Timeline
                 align="alternate"
                 className={viewSettings.view.value === "Horizontal" ? "horizontalTimeline" : "vertical"}
